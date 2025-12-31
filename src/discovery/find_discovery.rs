e49313627e03d0fbcb6f10e5efcb57f307ee70a4
sim-07 <121951559+sim-07@users.noreply.github.com>
2025-12-31 18:05:24 +0100

    modified:   src/discovery/find_discovery.rs

--- src/discovery/find_discovery.rs
+++ src/discovery/find_discovery.rs
@@ -6,10 +6,13 @@ pub async fn find_discovery(id: String) {
     let bytes = serde_json::to_vec(&disc_packet).unwrap();
 
     let mult_addr = "239.255.42.99:9000";
-    
+
     let udp_socket = UdpSocket::bind("0.0.0.0:0").await.unwrap();
 
-    udp_socket.set_broadcast(true).unwrap();    
-    udp_socket.send_to(&bytes, mult_addr).await.unwrap();
+    for _i in 0..3 {
+        udp_socket.set_broadcast(true).unwrap();
+        udp_socket.send_to(&bytes, mult_addr).await.unwrap();
 
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
 }
